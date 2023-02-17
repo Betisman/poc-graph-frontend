f7e1a76e7661296961e9bf9c824ddd67a30d8d59
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2023-02-17 15:35:48 +0100

    hardcode colors for the edges

--- src/Flow.js
+++ src/Flow.js
@@ -1,13 +1,16 @@
 import ReactFlow, {
-  Controls, Background, applyNodeChanges, applyEdgeChanges, addEdge, MiniMap,
+  Controls, Background, applyNodeChanges, applyEdgeChanges, addEdge, MiniMap, MarkerType,
 } from "reactflow";
 import 'reactflow/dist/style.css';
 import Axios from 'axios';
 import { useEffect, useState, useCallback } from "react";
+import * as d3 from 'd3';
+
 
 const nodesUrl = 'http://localhost:4000/api/node/Person';
 const edgesUrl = 'http://localhost:4000/api/edge';
 
+// ----------------------------------------------------
 const getRandomPosition = (minX, minY, maxX, maxY) => {
   const x = Math.random() * (maxX - minX) + minX;
   const y = Math.random() * (maxY - minY) + minY;
@@ -27,18 +30,86 @@ const isNodePositionColliding = (newNodePosition, nodes) => {
   }
   return false;
 };
+//---------------------------------------------------------
+const containerWidth = 1000;
+const containerHeight = 1000;
+const center = { x: containerWidth / 2, y: containerHeight / 2 };
+
+// const sortedNodes = nodes.sort((a, b) => a.id.localeCompare(b.id));
+// const angleSeparation = (2 * Math.PI) / sortedNodes.length;
+// const angleSeparation = nodes => (2 * Math.PI) / nodes.length;
+
+const radius = 700;
+const getCircularPosition = (node, index, nodesLength) => {
+  const angleSeparation = (2 * Math.PI) / nodesLength;
+  const angle = angleSeparation * index;
+  const x = radius * Math.cos(angle) + center.x;
+  const y = radius * Math.sin(angle) + center.y;
+
+  // Situar nodo en el diagrama
+  return { x, y };
+};
+//---------------------------------------------------------
+const generateCircleLayout2 = (nodes, radius) => {
+  // const renodes = nodes.map(n => ({ id: n.elementId, position: { x: 0, y: 0 }, name: n.elementId }))
+  // console.log('circle nodes', renodes)
+  const root = d3.pack().size([radius, radius])(d3.hierarchy({ children: nodes }).sum(() => 1));
+  const positions = {};
+  console.log(positions)
+  root.descendants().forEach((node) => {
+    
+    positions[node.data.elementId] = {
+      x: node.x - radius / 2,
+      y: node.y - radius / 2,
+    };
+  });
+  console.log('---', positions)
+  return positions;
+}
+
+function generateCircleLayout(nodes, radius) {
+  const centerX = radius;
+  const centerY = radius;
+
+  const simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(-100))
+    .force("x", d3.forceX(centerX))
+    .force("y", d3.forceY(centerY))
+    .force("collision", d3.forceCollide().radius(100))
+    .stop();
+
+  for (let i = 0; i < 300; i++) {
+    simulation.tick();
+  }
+
+  const positions = {};
+  nodes.forEach((node) => {
+    console.log(node)
+    positions[node.elementId] = {
+      x: node.x - radius / 2,
+      y: node.y - radius / 2,
+    };
+  });
+
+  return positions;
+}
+
+
+//---------------------------------------------------------
 
 const formatNodes = nodes => {
   const minX = 0;
   const minY = 0;
   const maxX = 1000;
   const maxY = 1000;
+  const positions = generateCircleLayout(nodes, 200);
   return nodes.map((node, index) => {
-    let newNodePosition = getRandomPosition(minX, minY, maxX, maxY);
-    while (isNodePositionColliding(newNodePosition, nodes)) {
-      newNodePosition = getRandomPosition(minX, minY, maxX, maxY);
-    }
-    console.log(newNodePosition);
+    // let newNodePosition = getRandomPosition(minX, minY, maxX, maxY);
+    // while (isNodePositionColliding(newNodePosition, nodes)) {
+    //   newNodePosition = getRandomPosition(minX, minY, maxX, maxY);
+    // }
+    // let newNodePosition = getCircularPosition(node, index, nodes.length);
+    let newNodePosition = positions[node.elementId];
     return {
       id: `${node.elementId}`,
       position: newNodePosition,
@@ -48,9 +119,17 @@ const formatNodes = nodes => {
   })
 };
 
+const edgeColors = {
+  operational: 'red',
+  hardskills: 'blue',
+  softskills: 'limegreen',
+}
 const formatEdges = edges => edges.map(edge => ({
   id: `${edge.elementId}:${edge.startNodeElementId}-${edge.endNodeElementId}`, source: `${edge.startNodeElementId}`, target: `${edge.endNodeElementId}`, label: `${edge.type}`, type: 'default',
-    markerEnd: { type: 'arrowclosed' },
+    markerEnd: { type: MarkerType.ArrowClosed },
+    style: {
+      stroke: edgeColors[edge.type.toLowerCase().replace(' ', '')]
+    }
 }));
 
 const Flow = () => {
